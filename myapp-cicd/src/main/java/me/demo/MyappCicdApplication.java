3d8849013646c71d922dc898817b78e88ca98621
Allen <n1702123@gmail.com>
2025-02-21 23:47:23 +0800

    init

---
+++ myapp-cicd/src/main/java/me/demo/MyappCicdApplication.java
@@ -0,0 +1,13 @@
+package me.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyappCicdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyappCicdApplication.class, args);
+    }
+
+}
